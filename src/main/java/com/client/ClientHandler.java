0d9cede74f5ef54c50595d18d1a8235b2ec1ea7e
Administrator <Administrator@PC201607051436>
2019-06-21 20:26:44 +0800

    断线重连

--- src/main/java/com/client/ClientHandler.java
+++ src/main/java/com/client/ClientHandler.java
@@ -1,3 +1,4 @@
+
 package com.client;
 /**
  * 客户端回调方法
@@ -6,6 +7,7 @@ package com.client;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,72 +18,91 @@ import com.kj.datacenter.msg.HeartMsg;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.EventLoop;
 import io.netty.channel.SimpleChannelInboundHandler;
 import test.Test1;
 
 class ClientHandler extends SimpleChannelInboundHandler {
-    private Logger log = LoggerFactory.getLogger(ClientHandler.class);
-    private static volatile boolean IsConnect = false;
-    private static Object locker = new Object();
-    private ExecutorService taskExecutor = Executors.newCachedThreadPool();
 
-    // 客户端连接服务器后被调用
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-	System.out.println("客户端连接服务器，开始心跳……");
-	synchronized (locker) {
-	    IsConnect = true;
+	private Logger log = LoggerFactory.getLogger(ClientHandler.class);
+	private static volatile boolean IsConnect = false;
+	private static Object locker = new Object();
+	private ExecutorService taskExecutor = Executors.newCachedThreadPool();
+
+	private DataServer client;
+
+	public ClientHandler(DataServer client) {
+		this.client = client;
 	}
-	this.startHeartBit(ctx);
 
-    }
-
-    private void startHeartBit(ChannelHandlerContext ctx) {
-	taskExecutor.execute(() -> {
-	    try {
-		HeartMsg bit = new HeartMsg();
-		bit.setDeviceId(Test1.c.getDeviceId());
-		byte[] req = JSON.toJSONString(bit).getBytes("UTF-8");
-		while (IsConnect) {
-		    ByteBuf firstMessage = Unpooled.buffer(req.length);
-		    firstMessage.writeBytes(req);
-		    ctx.writeAndFlush(firstMessage);
-		    try {
-			Thread.sleep(10000);
-		    } catch (Exception ex) {
-			continue;
-		    }
+	// 客户端连接服务器后被调用
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("客户端连接服务器，开始心跳……");
+		synchronized (locker) {
+			IsConnect = true;
+			SessionManager.getInstance().setCtx(ctx);
 		}
-	    } catch (Exception e) {
-		log.error(e.toString());
-	    }
-	});
+		this.startHeartBit(ctx);
 
-    }
-
-    // • 从服务器接收到数据后调用
-    @Override
-    protected void channelRead0(ChannelHandlerContext ctx, Object o) throws Exception {
-
-	System.out.println("client 读取server数据..");
-	// 服务端返回消息后
-	ByteBuf buf = (ByteBuf) o;
-	byte[] req = new byte[buf.readableBytes()];
-	buf.readBytes(req);
-	String body = new String(req, "UTF-8");
-	System.out.println("服务端数据为 :" + body);
-
-    }
-
-    // • 发生异常时被调用
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-	System.out.println("服务端断开连接...");
-	synchronized (locker) {
-	    IsConnect = false;
 	}
-	// 释放资源
-	ctx.close();
-    }
+
+	private void startHeartBit(ChannelHandlerContext ctx) {
+		taskExecutor.execute(() -> {
+			try {
+				HeartMsg bit = new HeartMsg();
+				bit.setDeviceId(Test1.c.getDeviceId());
+				byte[] req = JSON.toJSONString(bit).getBytes("UTF-8");
+				while (IsConnect) {
+					ByteBuf firstMessage = Unpooled.buffer(req.length);
+					firstMessage.writeBytes(req);
+					ctx.writeAndFlush(firstMessage);
+					try {
+						Thread.sleep(10000);
+					} catch (Exception ex) {
+						continue;
+					}
+				}
+			} catch (Exception e) {
+
+			}
+		});
+
+	}
+
+	// • 从服务器接收到数据后调用
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, Object o) throws Exception {
+
+		System.out.println("client 读取server数据..");
+		// 服务端返回消息后
+		ByteBuf buf = (ByteBuf) o;
+		byte[] req = new byte[buf.readableBytes()];
+		buf.readBytes(req);
+		String body = new String(req, "UTF-8");
+		System.out.println("服务端数据为 :" + body);
+
+	}
+
+	// • 发生异常时被调用
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		System.out.println("服务端断开连接...");
+		synchronized (locker) {
+			IsConnect = false;
+			SessionManager.getInstance().setCtx(null);
+			SessionManager.getInstance().setLogin(false);
+		}
+		// 重新连接
+		final EventLoop eventLoop = ctx.channel().eventLoop();
+		eventLoop.schedule(new Runnable() {
+
+			@Override
+			public void run() {
+				client.run();
+			}
+		}, 3L, TimeUnit.SECONDS);
+		super.channelInactive(ctx);
+	}
 
 }
--- src/main/java/com/client/DataServer.java
+++ src/main/java/com/client/DataServer.java
@@ -24,97 +24,88 @@ import io.netty.handler.timeout.IdleStateHandler;
 
 public final class DataServer implements Runnable {
 
-    private Logger log = LoggerFactory.getLogger(DataServer.class);
+	private Logger log = LoggerFactory.getLogger(DataServer.class);
 
-    private ExecutorService serverExecutor = Executors.newSingleThreadExecutor();
+	private ExecutorService serverExecutor = Executors.newSingleThreadExecutor();
 
-    private static volatile boolean running = false;
+	private static volatile boolean running = false;
 
-    /** The future. */
-    private Future<?> future;
+	/** The future. */
+	private Future<?> future;
 
-    private static Object locker = new Object();
+	private static Object locker = new Object();
 
-    private Config cfg;
+	private Config cfg;
 
-    public DataServer(Config cfg) {
-	this.cfg = cfg;
-	if (!DataServer.running) {
-	    start();
+	public DataServer(Config cfg) {
+		this.cfg = cfg;
+		if (!DataServer.running) {
+			start();
+		}
 	}
-    }
 
-    public void start() {
-	log.info("开启服务器......");
-	synchronized (locker) {
-	    this.running = true;
-	    future = serverExecutor.submit(this);
+	public void start() {
+		log.info("开启服务器......");
+		synchronized (locker) {
+			this.running = true;
+			future = serverExecutor.submit(this);
+		}
+		log.info("开启服务结束......");
 	}
-	log.info("开启服务结束......");
-    }
 
-    /**
-     * 停止锟斤拷锟斤拷.
-     */
-    public void stop() {
-	log.info("停止服务......");
-	synchronized (locker) {
-	    if (!running)
-		return;
+	/**
+	 * 停止锟斤拷锟斤拷.
+	 */
+	public void stop() {
+		log.info("停止服务......");
+		synchronized (locker) {
+			if (!running)
+				return;
 
-	    if (future == null)
-		return;
+			if (future == null)
+				return;
 
-	    future.cancel(running);
-	    future = null;
-	    this.running = false;
+			future.cancel(running);
+			future = null;
+			this.running = false;
+		}
+		log.info("停止服务结束......");
 	}
-	log.info("停止服务结束......");
-    }
 
-    public void run() {
-	EventLoopGroup nioEventLoopGroup = null;
-	try {
+	public void run() {
+		EventLoopGroup nioEventLoopGroup = null;
+		try {
 
-	    // 创建Bootstrap对象用来引导启动客户端
-	    Bootstrap bootstrap = new Bootstrap();
-	    // 创建EventLoopGroup对象并设置到Bootstrap中，EventLoopGroup可以理解为是一个线程池，这个线程池用来处理连接、接受数据、发送数据
-	    nioEventLoopGroup = new NioEventLoopGroup();
-	    // 创建InetSocketAddress并设置到Bootstrap中，InetSocketAddress是指定连接的服务器地址
-	    bootstrap.group(nioEventLoopGroup).channel(NioSocketChannel.class)
-		    .remoteAddress(new InetSocketAddress(this.cfg.getIp(), this.cfg.getPort()))
-		    .handler(new ChannelInitializer<SocketChannel>() {
-			// 添加一个ChannelHandler，客户端成功连接服务器后就会被执行
-			@Override
-			protected void initChannel(SocketChannel ch) throws Exception {
-			    ch.pipeline().addLast(new ClientHandler());
+			// 创建Bootstrap对象用来引导启动客户端
+			Bootstrap bootstrap = new Bootstrap();
+			// 创建EventLoopGroup对象并设置到Bootstrap中，EventLoopGroup可以理解为是一个线程池，这个线程池用来处理连接、接受数据、发送数据
+			nioEventLoopGroup = new NioEventLoopGroup();
+			// 创建InetSocketAddress并设置到Bootstrap中，InetSocketAddress是指定连接的服务器地址
+			bootstrap.group(nioEventLoopGroup).channel(NioSocketChannel.class)
+					.remoteAddress(new InetSocketAddress(this.cfg.getIp(), this.cfg.getPort()))
+					.handler(new ChannelInitializer<SocketChannel>() {
+
+						// 添加一个ChannelHandler，客户端成功连接服务器后就会被执行
+						@Override
+						protected void initChannel(SocketChannel ch) throws Exception {
+							ch.pipeline().addLast(new ClientHandler(DataServer.this));
+						}
+					});
+			bootstrap.connect().addListener(new ConnectionListener(this));
+			// • 调用Bootstrap.connect()来连接服务器
+			ChannelFuture f = bootstrap.connect().sync();
+			// • 最后关闭EventLoopGroup来释放资源
+			//f.channel().closeFuture().sync();
+		} catch (Exception ex) {
+
+		} finally {
+			try {
+				nioEventLoopGroup.shutdownGracefully().sync();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		    });
-	    // • 调用Bootstrap.connect()来连接服务器
-	    ChannelFuture f = bootstrap.connect().sync();
-	    // • 最后关闭EventLoopGroup来释放资源
-	    f.channel().closeFuture().sync();
-	} catch(Exception ex)
-	{
-	    
+		}
 	}
-	finally {
-	    try {
-		nioEventLoopGroup.shutdownGracefully().sync();
-	    } catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
-	}
-    }
 
-    private ChannelInitializer<SocketChannel> channelnit = new ChannelInitializer<SocketChannel>() {
-
-	@Override
-	protected void initChannel(SocketChannel channel) throws Exception {
-	    channel.pipeline().addLast("logging", new LoggingHandler(LogLevel.TRACE))
-		    .addLast("idle-handler", new IdleStateHandler(100, 100, 100))
-		    .addLast("client-handler", new ClientHandler());
-	}
-    };
 }
