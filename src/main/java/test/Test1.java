9f94dee3e6b9660b55003d564609a5ede10ccbfa
zlsf <zlsf@zlsf.lan>
2019-07-16 20:51:30 +0800

    获取实体

--- src/main/java/test/Test1.java
+++ src/main/java/test/Test1.java
@@ -4,19 +4,24 @@ import com.client.DataServer;
 import com.client.SessionManager;
 import com.config.Config;
 import com.kj.datacenter.msg.TestMessage;
+import com.kj.datacenter.msg.ZLSFTest;
 
 public class Test1 {
     public static Config c;
 
     public static void main(String[] args) {
 	c = new Config();
-	c.setIp("192.168.0.248");
+	c.setIp("127.0.0.1");
 	c.setPort(8899);
 	DataServer server = new DataServer(c);
 
 	try {
 	    Thread.sleep(10000);
-	    SessionManager.getInstance().SendMessage(new TestMessage());
+	    
+	    ZLSFTest zlsf=new ZLSFTest();
+	    zlsf.setName("name1111");
+	    zlsf.setSex("age1111");
+	    SessionManager.getInstance().SendMessage(zlsf);
 	} catch (Exception e) {
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
